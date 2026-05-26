d92c1036db430223a5dab04a5a6eeaec7944f555
viveksinghofficial <101392328+viveksinghofficial@users.noreply.github.com>
2026-05-26 18:32:53 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,7 @@
+//javascript for menu toggle
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
